aa400bf599e7ef42d411181c2251febcd8ea48b0
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-11-29 15:14:14 +0100

    function

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -8,13 +8,17 @@ c.font = fontSize + 'px Arial';
 const maxFuncXY = 10;
 const padding = 20;
 
-function getXYForValue(value) {
-    const in_min = maxFuncXY * -1;
-    const in_max = maxFuncXY;
-    const out_min = padding;
-    const out_max = canvasSize - padding;
-    // map range to other range
+
+function mapRange(value, in_min, in_max, out_min, out_max) {
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function getXForValue(value) {
+    return mapRange(value, maxFuncXY * -1, maxFuncXY, padding, canvasSize - padding);
+}
+
+function getYForValue(value) {
+    return mapRange(value, maxFuncXY * -1, maxFuncXY, canvasSize - padding, padding);
 }
 
 function drawLine(xStart, yStart, xEnd, yEnd) {
@@ -24,36 +28,48 @@ function drawLine(xStart, yStart, xEnd, yEnd) {
 }
 
 function drawAxes() {
-    const maxXY = getXYForValue(maxFuncXY);
-    const minXY = getXYForValue(maxFuncXY * -1);
-    const centerXY = getXYForValue(0);
+    const maxXY = getXForValue(maxFuncXY);
+    const minXY = getXForValue(maxFuncXY * -1);
+    const centerXY = getXForValue(0);
     c.fillText('X', padding / 2, centerXY);
     drawLine(minXY, centerXY, maxXY, centerXY);
 
     const startValueStepXY = centerXY - 5;
     const endValueStepXY = centerXY + 5;
-    for (let i = maxFuncXY * -1; i <= maxFuncXY; i++) {
-        let xValue = getXYForValue(i);
+    for (let x = maxFuncXY * -1; x <= maxFuncXY; x++) {
+        let xValue = getXForValue(x);
         drawLine(xValue, startValueStepXY, xValue, endValueStepXY);
-        if (i !== 0) {
-            c.fillText(i.toString(), xValue - fontSize / 2, endValueStepXY + fontSize);
+        if (x !== 0) {
+            c.fillText(x.toString(), xValue - fontSize / 2, endValueStepXY + fontSize);
         }
     }
 
     c.fillText('Y', centerXY, padding / 2);
     drawLine(centerXY, minXY, centerXY, maxXY);
-    for (let i = maxFuncXY * -1; i <= maxFuncXY; i++) {
-        let yValue = getXYForValue(i);
+    for (let y = maxFuncXY * -1; y <= maxFuncXY; y++) {
+        let yValue = getYForValue(y);
         drawLine(startValueStepXY, yValue, endValueStepXY, yValue);
-        if (i !== 0) {
-            c.fillText(i.toString(), endValueStepXY, yValue + fontSize / 2);
+        if (y !== 0) {
+            c.fillText(y.toString(), endValueStepXY, yValue + fontSize / 2);
         }
     }
     c.fillText('0', centerXY + fontSize / 2, centerXY + fontSize);
 }
 
+function clearCanvas() {
+    c.clearRect(0, 0, canvasSize, canvasSize);
+    c.beginPath();
+}
+
 function displayFunction(func) {
+    clearCanvas();
     drawAxes();
+    c.beginPath();
+    for (let x = maxFuncXY * -1; x <= maxFuncXY; x += 0.1) {
+        const y = eval(func.replaceAll('x', x));
+        c.lineTo(getXForValue(x), getYForValue(y));
+    }
+    c.stroke();
 }
 
 document.getElementById('form').addEventListener('submit', event => {
